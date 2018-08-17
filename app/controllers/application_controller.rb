954030332a5d6675202736da913b722babc781ce
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-17 15:52:00 +0000

    Update application_controller and update layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
+  end
 end
