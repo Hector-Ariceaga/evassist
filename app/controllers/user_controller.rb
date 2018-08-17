f541170d77402d021522fe79b2f94bf76d6cee5c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-17 16:48:23 +0000

    Add RESTful routes to user_controller, update user model and views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,46 @@
 class UserController < ApplicationController
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/new'
+  end
+
+  post '/signup' do
+    user = User.new(params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect '/events'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/events"
+    else
+      redirect "/"
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   has_many :tasks, through: :events
   has_secure_password
 
+  def slug
+    self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|instance| instance.slug == slug}
+  end
+
 end
