d7aad6399185e48d4e2ca8c931b4b4537fe2bdc5
TecHector <github email address>
2019-03-16 12:09:33 -0700

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,12 @@ require 'bundler/setup'
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV["SINATRA_ENV"]}.sqlite"
-)
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV["SINATRA_ENV"]}.sqlite"
+  )
+end
 
 configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
