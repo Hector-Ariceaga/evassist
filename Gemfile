359dc21c3b69b6175c551b14ad05a1cea48677e1
TecHector <github email address>
2019-03-17 14:05:50 -0700

    Deploy to Heroku

--- Gemfile
+++ Gemfile
@@ -1,16 +1,25 @@
 source "https://rubygems.org"
 
+ruby '2.6.2'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
 gem 'thin'
 gem 'bcrypt'
-gem 'sqlite3', '~> 1.3.6'
 gem 'shotgun'
 gem 'rack-flash3'
+
+
+group :development, :test do
+  gem 'sqlite3', '~> 1.3.6'
+end
 
 group :test do
   gem 'database_cleaner', '~> 1.7'
   gem 'pry'
   gem 'tux'
+end
+
+group :production do
+  gem 'pg'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,27 @@
-ENV["SINATRA_ENV"] ||= "development"
-
 require 'bundler/setup'
+require 'sinatra'
 require 'rack-flash'
-require 'database_cleaner'
+Bundler.require
 
-Bundler.require(:default, ENV['SINATRA_ENV'])
+configure :development do
+  ENV["SINATRA_ENV"] ||= "development"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV["SINATRA_ENV"]}.sqlite"
-)
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV["SINATRA_ENV"]}.sqlite"
+  )
+  end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 require_all 'app'
